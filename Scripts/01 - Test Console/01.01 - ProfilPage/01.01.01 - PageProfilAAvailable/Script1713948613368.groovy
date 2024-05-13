22ea16d1064af46541030fc3d4db92d039cd4bff
Nawel Mele <nawel.mele@reach5.co>
2024-05-13 10:18:56 +0200

    Modification login on Profil available test case

--- Scripts/01 - Test Console/01.01 - ProfilPage/01.01.01 - PageProfilAAvailable/Script1713948613368.groovy
+++ Scripts/01 - Test Console/01.01 - ProfilPage/01.01.01 - PageProfilAAvailable/Script1713948613368.groovy
@@ -21,13 +21,13 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl(GlobalVariable.ConsoleURL)
 
-WebUI.setEncryptedText(findTestObject('R5_console_Login/login_email'), GlobalVariable.Login_NME)
+WebUI.setText(findTestObject('R5_console_Login/login_email'), GlobalVariable.Login_NME)
 
-WebUI.click(findTestObject('R5_console_Login/login_password'))
+WebUI.setEncryptedText(findTestObject('R5_console_Login/login_password'), '5rdcrhgBYgE/1Hb7+0nwZQ==')
+
+WebUI.click(findTestObject('R5_console_Login/button_Log in'))
 
 WebUI.click(findTestObject('Object Repository/ProfilePage/div_Go to account_css-8yile7-indicatorContainer'))
-
-WebUI.setText(findTestObject('R5_console_Login/login_email'), GlobalVariable.Login_NME)
 
 WebUI.click(findTestObject('Object Repository/HP/div_Integ-QA-fonctionnelle'))
 
