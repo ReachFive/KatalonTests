d04aaf5cdc368dd33aa56b4fb4fd4d57385c814b
Nawel Mele <nawel.mele@reach5.co>
2024-07-01 10:10:56 +0200

    test push

---
+++ Scripts/01 - Test Console/01.01 - ProfilPage/01.01.02 - ProfileSearch/Script1719821429833.groovy
@@ -0,0 +1,75 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
+import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
+import org.openqa.selenium.WebElement as WebElement
+
+/**
+ Comment for Git
+ **/
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(GlobalVariable.ConsoleURL)
+
+WebUI.setText(findTestObject('R5_console_Login/login_email'), GlobalVariable.Login_NME)
+
+WebUI.setEncryptedText(findTestObject('R5_console_Login/login_password'), '5rdcrhgBYgE/1Hb7+0nwZQ==')
+
+WebUI.click(findTestObject('R5_console_Login/button_Log in'))
+
+WebUI.click(findTestObject('Object Repository/HP/Page_Console - ReachFive/div_Go to account'))
+
+/*WebUI.setText(findTestObject('Object Repository/HP/Page_Console - ReachFive/input_Go to account_react-select-2-input'), 
+    'nme')*/
+WebUI.scrollToElement(findTestObject('HP/div_integ-qa-nme'), 3)
+
+WebUI.click(findTestObject('HP/div_integ-qa-nme'))
+
+WebUI.click(findTestObject('Object Repository/ProfilePage/span_Profiles'))
+
+WebUI.click(findTestObject('Object Repository/ProfilePageSearch/SearchProfilFilter'))
+
+WebUI.setText(findTestObject('Object Repository/ProfilePageSearch/SearchProfilFilter'), 'KatlonSPGivenName  KatlonSPFamilyName')
+
+WebUI.delay(5)
+
+WebUI.verifyTextPresent('KatlonSPGivenName KatlonSPFamilyName', false)
+
+WebUI.setText(findTestObject('Object Repository/ProfilePageSearch/SearchProfilFilter'), 'katalon_search_profile@gmail.com')
+
+WebUI.delay(5)
+
+WebUI.verifyTextPresent('katalon_search_profile@gmail.com', false)
+
+WebUI.setText(findTestObject('Object Repository/ProfilePageSearch/SearchProfilFilter'), '+33634443795')
+
+WebUI.delay(5)
+
+WebUI.verifyTextPresent('katalon_search_profile@gmail.com', false)
+
+WebUI.setText(findTestObject('Object Repository/ProfilePageSearch/SearchProfilFilter'), 'AY93l4giKOIS0VrJBqFH')
+
+WebUI.delay(5)
+
+WebUI.verifyTextPresent('katalon_search_profile+2@gmail.com', false)
+
+WebUI.delay(1)
+
+WebUI.closeBrowser()
+
